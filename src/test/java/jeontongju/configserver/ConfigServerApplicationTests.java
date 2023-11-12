4ab0af8b385d093cf31e9c1c78d453f26e25287f
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-11-12 19:52:34 +0900

    first commit

---
+++ src/test/java/jeontongju/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jeontongju.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
